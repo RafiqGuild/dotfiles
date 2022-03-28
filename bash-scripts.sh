c07e9281025f125cebb6cb1b917c1c0181965b8f
Rafiq <89995535+RafiqGuild@users.noreply.github.com>
2022-03-28 13:31:56 -0600

    Update bash-scripts.sh

--- bash-scripts.sh
+++ bash-scripts.sh
@@ -5,20 +5,27 @@ source ~/docker-aliases.sh
 alias dev='aws2-wrap --profile guild-dev'
 alias staging='aws2-wrap --profile guild-staging'
 alias prod='aws2-wrap --profile guild-prod'
-alias aws-login='aws sso login --profile=guild-dev && aws sso login --profile=guild-staging && aws sso login --profile=guild-prod'
+
+alias dev-export='eval "$(aws2-wrap --profile guild-dev --export)"'
+alias staging-export='eval "$(aws2-wrap --profile guild-staging --export)"'
+alias prod-export='eval "$(aws2-wrap --profile guild-prod --export)"'
+
+function aws-login() {
+    aws sso login --profile=guild-dev --profile=guild-staging --profile=guild-prod
+}
 
 function aws-dev() {
-	aws $@ --profile guild-dev
+    aws $@ --profile guild-dev
 }
 
 function aws-staging() {
-	aws $@ --profile guild-staging
+    aws $@ --profile guild-staging
 }
 function aws-prod() {
-	aws $@ --profile guild-prod
+    aws $@ --profile guild-prod
 }
 
 # search history
 hg() {
-	history | grep -i $1
+    history | grep -i $1
 }
